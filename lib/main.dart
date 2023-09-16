1f606dd9a69ea3fefd86af1b391a130dde76aefe
Silvio Daniel Arzeno <silviodanielarzeno@gmail.com>
2023-09-16 19:44:24 -0400

    fix: Binding instance getting called without ensureInitialized

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
