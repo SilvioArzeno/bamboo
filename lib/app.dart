f9d2d8682f98a86f34ff1a8c11a6457eeff92560
Silvio Daniel Arzeno <silviodanielarzeno@gmail.com>
2023-09-18 23:39:42 -0400

    feat(auth): add default styles to app

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,6 @@
 import 'package:bamboo_finances/src/features/auth/presentation/pages/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
@@ -7,7 +8,28 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(useMaterial3:true, primaryColor: Colors.white,,),
+      theme: ThemeData(
+        useMaterial3: true,
+        primaryColor: Colors.white,
+        primaryTextTheme: TextTheme(
+          headlineMedium: GoogleFonts.montserrat(
+              textStyle: const TextStyle(
+                  color: Color(0xFF058C42),
+                  fontSize: 42,
+                  fontWeight: FontWeight.bold)),
+        ),
+      ),
+      darkTheme: ThemeData(
+        useMaterial3: true,
+        primaryColor: const Color(0xFF0D2818),
+        primaryTextTheme: TextTheme(
+          headlineMedium: GoogleFonts.montserrat(
+              textStyle: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 42,
+                  fontWeight: FontWeight.bold)),
+        ),
+      ),
       home: const Scaffold(body: LoginPage()),
     );
   }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,8 +7,10 @@ import Foundation
 
 import firebase_core
 import firebase_crashlytics
+import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseCrashlyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCrashlyticsPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
